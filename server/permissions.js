6a5387eec08d8363ab828d03d286d8560aca004a
Anphr <anphrr@gmail.com>
2024-04-21 21:59:42 +0100

    First commit

---
+++ server/permissions.js
@@ -0,0 +1,38 @@
+module.exports = [
+    {
+        "key": process.env.TOKEN_1,
+        "discordID": "1097982903348117626",
+        "nameColor": "#000000",
+        "class": "developer",
+        "infiniteLevelUp": true,
+        "nameParts": "anphr", // Each part of the name
+        "note": "DEV"
+    },
+    {
+        "key": process.env.TOKEN_2,
+        "discordID": "981618757019525130",
+        "nameColor": "#ff9900",
+        "class": "developer",
+        "infiniteLevelUp": true,
+        "name": "aminium",
+        "note": "DEV"
+    },
+    {
+        "key": process.env.TOKEN_3,
+        "discordID": "0",
+        "nameColor": "#ffffff",
+        "class": "developer",
+        "infiniteLevelUp": true,
+        "name": "unnamed#0000",
+        "note": "note here"
+    },
+    {
+        "key": process.env.TOKEN_4,
+        "discordID": "0",
+        "nameColor": "#ffffff",
+        "class": "developer",
+        "infiniteLevelUp": true,
+        "name": "unnamed#0000",
+        "note": "note here"
+    },
+]
